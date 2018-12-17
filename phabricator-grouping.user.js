7bd14d4ca4f83dce75b3d46397bfc4884a1f2624
Payton Swick <payton@foolord.com>
2018-12-17 12:51:30 -0500

    Bump version to 1.0.1

--- phabricator-grouping.user.js
+++ phabricator-grouping.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Phabricator Notification Grouping
 // @namespace    https://github.com/sirbrillig/phabricator-grouping
-// @version      1.0
+// @version      1.0.1
 // @description  Allows collapsing Phabricator notifications to one-per-revision
 // @author       Payton Swick <payton@foolord.com>
 // @match        https://code.a8c.com/notification/*
